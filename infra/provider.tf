f7182c0af87d09f50bc433af4bd0fc62b3002e62
glennbech <glenn.bech@gmail.com>
2023-12-17 16:56:25 +0100

    Go sam! sam!

--- infra/provider.tf
+++ infra/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket = "pgr301-2021-terraform-state"
-    key    = "candidate2043bucketkey/apprunner-a-new-state.state"
+    key    = "candidate2043bucketkey/apprunner-another-new-state.state"
     region = "eu-north-1"
   }
 }
